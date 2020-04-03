211fd66a544f33c020fb9d0f403e8ad9e8c33ba3
GYU WON CHO <whrbdnjs33@naver.com>
2020-04-03 18:45:35 +0900

    테스터를 발전시킴
    수치들을 바꾸고 테스트에 돌입하는 주기를
    빠르게 할수있도록 상수보다 변수중심으로 설계

--- 판정테스트.py
+++ 판정테스트.py
@@ -1,14 +1,23 @@
 import pyautogui
 import time
-#1862 660
+
 
 def isAchromatic(x, y):
     if(pyautogui.pixel(x, y) == (16, 24, 27) or pyautogui.pixel(x, y) == (22, 32, 33) or pyautogui.pixel(x, y) == (0, 0, 0) or pyautogui.pixel(x, y) == (255, 255, 255)):
         return False
     avgVal = (pyautogui.pixel(x, y)[0] + pyautogui.pixel(x, y)[1] + pyautogui.pixel(x, y)[2]) / 3
-    if abs(pyautogui.pixel(x, y)[0] - avgVal) <= 20 and abs(pyautogui.pixel(x, y)[1] - avgVal) <= 20 and abs(pyautogui.pixel(x, y)[2] - avgVal) <= 20:
+    if abs(pyautogui.pixel(x, y)[0] - avgVal) <= val and abs(pyautogui.pixel(x, y)[1] - avgVal) <= val and abs(pyautogui.pixel(x, y)[2] - avgVal) <= val:
         return True
     else:
+        return False
+
+
+def isColor(x, y, rank):
+    avgVal = (pyautogui.pixel(x, y - 72 * (8-rank))[0] + pyautogui.pixel(x, y - 72 * (8-rank))[1] + pyautogui.pixel(x, y - 72 * (8-rank))[2]) / 3
+    if abs(pyautogui.pixel(x, y - 72 * (8-rank))[0] - avgVal) > val or abs(pyautogui.pixel(x, y - 72 * (8-rank))[1] - avgVal) > val or abs(pyautogui.pixel(x, y - 72 * (8-rank))[2] - avgVal) > val:
+        return True
+    else:
+        print("%i등이 문제" %rank)
         return False
 
 
@@ -18,8 +27,15 @@ def isSix():
     else:
         return False
 
+def test():
+    if isColor(x, y, 8) and isColor(x, y, 7) and isColor(x, y, 6) and isColor(x, y, 5) and isColor(x, y, 4) and isColor(x, y, 3) and isColor(x, y, 2) and isColor(x, y, 1):
+        print("pass!")
+    else:
+        print("fail")
 
-
+x = 1874
+y = 742
+val = 10
 time.sleep(5)
 while True:
-    print(isSix())
+    test()
