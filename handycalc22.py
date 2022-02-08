8b5e05c94121e87e6715f8475902545b29e2219a
qone2 <whrbdnjs33@naver.com>
2022-02-09 03:32:39 +0900

    필요없는 부분 삭제

--- handycalc22.py
+++ handycalc22.py
@@ -4,8 +4,6 @@ import time
 import sys
 import ctypes
 import subprocess
-import urllib.parse
-import json
 import msvcrt
 
 # direct inputs
@@ -668,69 +666,6 @@ def game_regame():
     """
     게임종료 후 통계창에서 한판더하기 클릭
     """
-    global bot_players
-    print("한판더하기")
-    if hybrid_switch[0] > 0:
-        with open("token.txt", 'r', encoding="utf8") as token_txt:
-            summoner_token, summoner_name, exceptions = token_txt.read().split('\n')
-        exceptions = list(exceptions.replace(' ', '').split(','))
-        custom_headers = {
-            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36",
-            "Accept-Language": "ko-KR,ko;q=0.9,en-US;q=0.8,en;q=0.7",
-            "Accept-Charset": "application/x-www-form-urlencoded; charset=UTF-8",
-            "Origin": "https://developer.riotgames.com",
-            "X-Riot-Token": summoner_token
-        }
-
-        riot_url1 = "https://kr.api.riotgames.com/tft/summoner/v1/summoners/by-name/"+urllib.parse.quote(summoner_name)
-        res1 = requests.get(riot_url1, headers=custom_headers)
-        if res1.status_code != 200:
-            click(863, 844)
-            time.sleep(1)
-            return
-        time.sleep(0.05)
-        summoner_puuid = res1.json()["puuid"]
-
-        riot_url2 = "https://asia.api.riotgames.com/tft/match/v1/matches/by-puuid/" + summoner_puuid + "/ids?count=5"
-        res2 = requests.get(riot_url2, headers=custom_headers)
-        if res2.status_code != 200:
-            click(863, 844)
-            time.sleep(1)
-            return
-        time.sleep(0.05)
-        match_ids = res2.json()
-
-        for match_id in match_ids:
-            riot_url3 = "https://asia.api.riotgames.com/tft/match/v1/matches/" + match_id
-            res3 = requests.get(riot_url3, headers=custom_headers)
-            if res3.status_code != 200:
-                click(863, 844)
-                time.sleep(1)
-            time.sleep(0.05)
-            res3 = res3.json()
-
-            with open("bot_players.json", 'r', encoding="utf8") as bot_players_json:
-                bot_players = json.load(bot_players_json)
-
-            for player in res3["info"]["participants"]:
-                if player["time_eliminated"] < 780:
-                    riot_url4 = "https://kr.api.riotgames.com/tft/summoner/v1/summoners/by-puuid/" + player["puuid"]
-                    res4 = requests.get(riot_url4, headers=custom_headers)
-                    if res4.status_code != 200:
-                        click(863, 844)
-                        time.sleep(1)
-                    time.sleep(0.05)
-                    player_name = res4.json()["name"].replace(' ', '')
-                    if (player_name in bot_players
-                            and player_name not in exceptions
-                            and player["time_eliminated"] not in bot_players[player_name]):
-                        bot_players[player_name].append(player["time_eliminated"])
-                    elif player_name not in exceptions:
-                        bot_players[player_name] = [player["time_eliminated"]]
-
-        with open("bot_players.json", 'w', encoding="utf8") as bot_players_json:
-            json.dump(bot_players, bot_players_json, indent=4, ensure_ascii=False)
-
     click(863, 844)
     time.sleep(1)
 
@@ -830,13 +765,8 @@ def mode_select():
     print("6. 모드 설명")
     print("7. 종료")
     print("8. 끝날때까지")
-    print("9. hybrid-under11")
-    print("10. hybrid-under12")
-    print("11. hybrid-under13")
     try:
         mode[0] = int(input())
-        if 9 <= mode[0] <= 11:
-            hybrid_switch[0] = 0
     except ValueError:
         print("인풋에러, 올바른 값을 입력하시오")
         mode[0] = 0
@@ -910,7 +840,6 @@ Yserr = 737
 devMode = [False]
 overs = [0]
 wins = [0]
-hybrid_switch = [0]
 load_profile_got = [False]
 hybrid_imported = [False]
 
@@ -921,8 +850,7 @@ if __name__ == "__main__":
     while True:
         if mode[0] == 0:
             mode_select()
-        elif (mode[0] == 1 or mode[0] == 2 or mode[0] == 3 or mode[0] == 4 or mode[0] == 8
-              or (1 <= hybrid_switch[0] <= 3)):
+        elif mode[0] == 1 or mode[0] == 2 or mode[0] == 3 or mode[0] == 4 or mode[0] == 8:
             try:
                 handycalc()
             except pyautogui.FailSafeException:
@@ -933,7 +861,6 @@ if __name__ == "__main__":
 3. 버그시 강제 서렌
 4. 모드 변경
 5. 개발자모드On/Off
-6. hybrid 스위치 Off
 """))
                 if exmenu == 1:
                     "게임으로 돌아갑니다."
@@ -948,9 +875,6 @@ if __name__ == "__main__":
                     mode_select()
                 elif exmenu == 5:
                     dev_switch()
-                elif exmenu == 6:
-                    hybrid_switch[0] = 0
-                    mode[0] = 0
                 else:
                     print("이상한 값 입력. 종료합니다.")
                     time.sleep(2)
@@ -964,22 +888,7 @@ if __name__ == "__main__":
             print("종료")
             time.sleep(2)
             break
-        elif 9 <= mode[0] <= 11:
-            if hybrid_imported[0] is False:
-                import cv2
-                import numpy as np
-                import pytesseract
-                import requests
-
-                pytesseract.pytesseract.tesseract_cmd = 'C:\\Program Files (x86)\\Tesseract-OCR\\tesseract.exe'
-                hybrid_imported[0] = True
-            hybrid_switch[0] = mode[0] - 8
-            mode[0] = 3
         else:
             print("올바른 값을 입력하십시오.")
             mode[0] = 0
             mode_select()
-
-# =====================================data======================================
-
-    bot_players = []
